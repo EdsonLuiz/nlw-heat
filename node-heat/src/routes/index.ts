e1f394afe2d1a01be62ff9d5cef7d772142c8419
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2021-10-19 16:30:49 -0300

    feat: save message on DB

--- node-heat/src/routes/index.ts
+++ node-heat/src/routes/index.ts
@@ -1,8 +1,11 @@
 import {Router} from 'express'
 import { AuthenticateUserController } from '../controllers/authenticate-user-controller'
+import { CreateMessageController } from '../controllers/create-message-controller'
+import { ensureAuthenticated } from '../middleware/ensure-authenticated'
 
 const router = Router()
 
 router.post('/authenticate', new AuthenticateUserController().handle)
+router.post('/messages', ensureAuthenticated, new CreateMessageController().handle)
 
 export {router}
