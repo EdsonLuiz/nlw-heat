cbced520b6e0fa9e8e4c649518f7e5021f2c855d
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2021-10-21 19:54:18 -0300

    feat: improve UI with motion

--- web/src/components/login-box/index.tsx
+++ web/src/components/login-box/index.tsx
@@ -1,14 +1,34 @@
-import { Button, Text, VStack } from "@chakra-ui/react";
+import { Button, Text } from "@chakra-ui/react";
+import {VStack, StackProps} from '@chakra-ui/layout'
+import {motion} from 'framer-motion'
 import {VscGithubInverted} from 'react-icons/vsc'
 
+const vstackVariant = {
+  initial: {
+    opacity: 0,
+  },
+  animate: {
+    opacity: 1,
+    transition: {
+      duration: 1,
+      delay: 0.4
+    }
+  }
+}
+
 export function LoginBox() {
+  const MotionVStack = motion<StackProps>(VStack)
   return (
-    <VStack h="100vh" w="full" p="440px 80px 0"
+    <MotionVStack h="100vh" w="full" p="440px 80px 0"
             justify="center" align="center"
             backgroundImage="/banner-girl.png"
             backgroundRepeat="no-repeat"
             backgroundPosition="center top"
-            backgroundColor="gray.800">
+            backgroundColor="gray.800"
+            variants={vstackVariant}
+            initial="initial"
+            animate="animate"
+            >
       <Text as="strong" textAlign="center" fontSize="3xl" fontWeight="700" lineHeight="xl">
         Entre e compartilhe sua mensagem
       </Text>
@@ -16,6 +36,6 @@ export function LoginBox() {
               leftIcon={<VscGithubInverted size={24} />}>
       Entrar com Github
       </Button>
-    </VStack>
+    </MotionVStack>
   )
 }
