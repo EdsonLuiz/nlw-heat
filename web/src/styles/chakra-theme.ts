93f269ffad006aa13652f959f47ec6ba10ddfdb6
Edson Luiz Ribeiro Rodrigues <edsonrodrigues@icloud.com>
2021-10-22 13:21:43 -0300

    refactor: add brandYellow color theme

--- web/src/styles/chakra-theme.ts
+++ web/src/styles/chakra-theme.ts
@@ -2,9 +2,17 @@ import {extendTheme, theme as base} from '@chakra-ui/react'
 
 export const theme = extendTheme({
   colors: {
-    brandButton: {
-      200: '#ffcd1e',
-      300: '#f6e05e'
+    "brandYellow": {
+      "50": "#FFF9E5",
+      "100": "#FFEFB8",
+      "200": "#FFE58A",
+      "300": "#FFDB5C",
+      "400": "#FFD12E",
+      "500": "#FFC600",
+      "600": "#CC9F00",
+      "700": "#997700",
+      "800": "#664F00",
+      "900": "#332800"
     }
   },
   fonts: {
